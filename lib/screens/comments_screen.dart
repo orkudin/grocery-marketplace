836cd61dc075da0f11549a71f0261afaa8ee862f
orkudin <106586367+orkudin@users.noreply.github.com>
2024-04-23 08:32:43 +0500

    first version

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -11,7 +11,7 @@ class _ReviewScreenState extends State<ReviewScreen> {
     [
       {
         "name": "Иван Петров",
-        "rating": 4.5,
+        "rating": 4.0,
         "comment": "Отличный продукт, работает как надо.",
         "pros": "Высокое качество, удобный интерфейс",
         "cons": "Немного дороговат",
@@ -19,7 +19,7 @@ class _ReviewScreenState extends State<ReviewScreen> {
       },
       {
         "name": "Анна Сидорова",
-        "rating": 3.8,
+        "rating": 3.0,
         "comment": "В целом неплохо, но есть недочеты.",
         "pros": "Быстрая доставка",
         "cons": "Не хватает некоторых функций",
@@ -27,10 +27,18 @@ class _ReviewScreenState extends State<ReviewScreen> {
       }
     ]
   ''';
+  final _formKey = GlobalKey<FormState>();
 
   List<dynamic> _reviews = [];
   int _selectedReviewIndex = -1;
   String _replyText = '';
+
+  String _newComment = '';
+  String _newPros = '';
+  String _newCons = '';
+  double _newRating = 0;
+
+  bool _isCommenting = false;
 
   @override
   void initState() {
@@ -59,9 +67,31 @@ class _ReviewScreenState extends State<ReviewScreen> {
     });
   }
 
+  void _submitReview() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _reviews.add({
+          'name': 'Покупатель',
+          'rating': _newRating,
+          'comment': _newComment,
+          'pros': _newPros,
+          'cons': _newCons,
+          'reply': null,
+        });
+        _newComment = '';
+        _newPros = '';
+        _newCons = '';
+        _newRating = 0;
+      });
+    }
+    _isCommenting = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    double averageRating = _reviews.fold(0.0, (sum, review) => sum + review['rating']) / _reviews.length;
+    double averageRating =
+        _reviews.fold(0.0, (sum, review) => sum + review['rating']) /
+            _reviews.length;
 
     return Scaffold(
       appBar: AppBar(
@@ -101,7 +131,8 @@ class _ReviewScreenState extends State<ReviewScreen> {
                           children: [
                             Text(
                               review['name'],
-                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
                             ),
                             SizedBox(height: 4),
                             Row(
@@ -122,7 +153,8 @@ class _ReviewScreenState extends State<ReviewScreen> {
                             SizedBox(height: 8),
                             Text(
                               'Достоинства: ${review['pros']}',
-                              style: TextStyle(fontSize: 14, color: Colors.green),
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.green),
                             ),
                             Text(
                               'Недостатки: ${review['cons']}',
@@ -179,6 +211,120 @@ class _ReviewScreenState extends State<ReviewScreen> {
               },
             ),
           ),
+          if (!_isCommenting)
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _isCommenting = true;
+                  });
+                },
+                child: Text('Оставить отзыв')),
+          if (_isCommenting)
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Оставить отзыв',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            setState(() {
+                              _isCommenting = false;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _newComment = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Введите отзыв';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Введите ваш комментарий',
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _newPros = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Введите отзыв';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Введите достоинства',
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _newCons = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Введите отзыв';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Введите недостатки',
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Text('Оценка: '),
+                        Expanded(
+                          child: Slider(
+                            value: _newRating,
+                            min: 0,
+                            max: 5,
+                            divisions: 5,
+                            onChanged: (value) {
+                              setState(() {
+                                _newRating = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Text(_newRating.toStringAsFixed(1)),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: _submitReview,
+                      child: Text('Оставить отзыв'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
         ],
       ),
     );
